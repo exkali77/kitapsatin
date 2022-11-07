0500927a34319c458342f3f41b0fb67d5ad84a8d
Kerem Şenses <68199514+exkali77@users.noreply.github.com>
2022-11-07 22:22:12 +0300

    Kitapsatınalma
    C#  Kitap adetini girdiğin de fiyatı hesaplayan program

---
+++ Kitap_indirimi/Kitap_indirimi/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kitap_indirimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Kitapadet=Convert.ToInt32(textBox1.Text);
+            double toplam;
+            if (Kitapadet > 0 && Kitapadet <= 20)
+            {
+                toplam = (Kitapadet * 8) - (Kitapadet * 8 * 0.20);
+                label3.Text = toplam + " TL";
+            }
+         
+            if (Kitapadet>=21 && Kitapadet <=40)
+            {
+                toplam = (Kitapadet * 8)  - (Kitapadet * 8 * 0.40);
+                label3.Text = toplam + " TL";
+            }  
+            if (Kitapadet>=41)
+            {
+                toplam = (Kitapadet * 8)  - (Kitapadet * 8 * 0.50);
+                label3.Text = toplam + " TL";
+            }
+        }
+    }
+}
